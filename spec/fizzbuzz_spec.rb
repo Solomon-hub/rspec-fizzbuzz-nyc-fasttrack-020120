bd0679aff161dca226b81aed4f128b2db444e7bd
Solomon Chu <solomonchu@live.com>
2020-02-05 04:01:22 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -25,12 +25,3 @@ describe "fizzbuzz" do
   end
 end
 
-def fizzbuzz(int)
-  if int % 3 == 0 and int % 5 == 0
-    puts "FizzBuzz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 == 0
-    puts  "Fizz"
-  end
-end
