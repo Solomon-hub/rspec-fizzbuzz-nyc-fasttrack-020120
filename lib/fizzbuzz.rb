26f1a65de15d1e48a6e1be4032cb5ce9cbcc159b
Solomon Chu <solomonchu@live.com>
2020-02-05 04:01:11 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,9 @@
-def fizzbuzz
-  if fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0 and int % 5 == 0
+    puts "FizzBuzz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 3 == 0
+    puts  "Fizz"
+  end
+end
